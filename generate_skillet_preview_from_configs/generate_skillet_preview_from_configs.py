35ef3efed29fe0033b3fa8c2326791e4be0654ce
Nathan <nembery@gmail.com>
2019-12-17 15:59:09 -0500

    attempt to only replace items from parent xpath

--- generate_skillet_preview_from_configs/generate_skillet_preview_from_configs.py
+++ generate_skillet_preview_from_configs/generate_skillet_preview_from_configs.py
@@ -3,6 +3,7 @@ from skilletlib import Panoply
 import os
 import sys
 import re
+from xml.etree import ElementTree
 
 # grab our two configs from the environment
 base_config_path = os.environ.get('BASE_CONFIG', '/Users/nembery/PycharmProjects/skilletlib/local_t/generate_skillet/config-01.xml')
@@ -26,12 +27,20 @@ if len(snippets) == 0:
     print('No Candidate Configuration can be found to use to build a skillet!')
     sys.exit(2)
 
-
+latest_doc = ElementTree.fromstring(latest_config)
 latest_config_html = latest_config.replace('<', '&lt;').replace('>', '&gt;')
 for s in snippets:
+    xpath = s.get('xpath').replace('/config/', './')
+    parent_element_xpath = "/".join(xpath.split('/')[:-1])
+    parent_element = latest_doc.find(parent_element_xpath)
+    parent_element_string = ElementTree.tostring(parent_element).decode('UTF-8')\
+        .replace('<', '&lt;').replace('>', '&gt;')
+
     element_html = s.get('element', '').replace('<', '&lt;').replace('>', '&gt;')
     element_wrapped = f"<span class='text-danger'>{element_html}</span>"
-    latest_config_html = latest_config_html.replace(element_html, element_wrapped)
+    parent_element_html = parent_element_string.replace(element_html, element_wrapped)
+
+    latest_config_html = latest_config_html.replace(parent_element_string, parent_element_html)
 
 print('<code type="xml">')
 print(latest_config_html)
