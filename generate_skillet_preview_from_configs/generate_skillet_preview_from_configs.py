af146cd7402a70d3b631dcda117786a9dcf805fd
Nathan <nembery@gmail.com>
2019-12-17 16:07:27 -0500

    update outputs, fix xpath replace

--- generate_skillet_preview_from_configs/generate_skillet_preview_from_configs.py
+++ generate_skillet_preview_from_configs/generate_skillet_preview_from_configs.py
@@ -6,8 +6,8 @@ import re
 from xml.etree import ElementTree
 
 # grab our two configs from the environment
-base_config_path = os.environ.get('BASE_CONFIG', '/Users/nembery/PycharmProjects/skilletlib/local_t/generate_skillet/config-01.xml')
-latest_config_path = os.environ.get('LATEST_CONFIG', '/Users/nembery/PycharmProjects/skilletlib/local_t/generate_skillet/config-02.xml')
+base_config_path = os.environ.get('BASE_CONFIG', '/Users/nembery/Downloads/sdwan_stage1.xml')
+latest_config_path = os.environ.get('LATEST_CONFIG', '/Users/nembery/Downloads/sdwan_final_test.xml')
 
 with open(base_config_path, 'r') as bcf:
     base_config = bcf.read()
@@ -30,14 +30,15 @@ if len(snippets) == 0:
 latest_doc = ElementTree.fromstring(latest_config)
 latest_config_html = latest_config.replace('<', '&lt;').replace('>', '&gt;')
 for s in snippets:
-    xpath = s.get('xpath').replace('/config/', './')
+    full_xpath = s.get('xpath')
+    xpath = re.sub('^/config', './', full_xpath)
     parent_element_xpath = "/".join(xpath.split('/')[:-1])
     parent_element = latest_doc.find(parent_element_xpath)
     parent_element_string = ElementTree.tostring(parent_element).decode('UTF-8')\
         .replace('<', '&lt;').replace('>', '&gt;')
 
     element_html = s.get('element', '').replace('<', '&lt;').replace('>', '&gt;')
-    element_wrapped = f"<span class='text-danger'>{element_html}</span>"
+    element_wrapped = f"<span class='text-danger' title='{full_xpath}'>{element_html}</span>"
     parent_element_html = parent_element_string.replace(element_html, element_wrapped)
 
     latest_config_html = latest_config_html.replace(parent_element_string, parent_element_html)
